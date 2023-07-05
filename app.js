f03dd4678d468706d9431665c410135627d51a5e
Elias Fakoorian <98291494+seyedeliasfakoorian@users.noreply.github.com>
2023-07-05 10:57:02 -0700

    app.js

--- app.js
+++ app.js
@@ -3,60 +3,60 @@ const express = require("express"),
   app = express(),
   http = require("http"),
   fs = require("fs"),
-  path = require("path");
+  path = require("path")
 
 const config = JSON.parse(
   fs.readFileSync("./config.json", {
     encoding: "utf8"
   })
-);
+)
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 
 const localprox = new alloy({
   prefix: "/prefix/",
   error: (proxy) => {
     return proxy.res.send(
       fs.readFileSync(path.join(__dirname, "public", "error.html"), "utf8")
-    );
+    )
   },
   request: [],
   response: [],
-  injection: true
-});
+  injection: True
+})
 
-app.use(localprox.app);
+app.use(localprox.app)
 
-localprox.ws(server);
+localprox.ws(server)
 
 //Cloudflare Attack Mode Fix
 
 app.post("/", async (req, res) => {
-  switch (req.url) {
+  switch (req.url:
     case "/":
       return res.send(
         fs.readFileSync(path.join(__dirname, "public", "index.html"), "utf8")
-      );
+      )
   }
-});
+})
 
 //Querystring Navigation
 app.get("/", async (req, res) => {
-  switch (req.url) {
+  switch (req.url:
     case "/":
       return res.send(
         fs.readFileSync(path.join(__dirname, "public", "index.html"), "utf8")
-      );
+      )
   }
 
-  switch (req.url) {
+  switch (req.url:
     case "/?a":
       return res.send(
         fs.readFileSync(path.join(__dirname, "public", "error.html"), "utf8")
-      );
+      )
   }
-});
+})
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")))
 
-server.listen(process.env.PORT || config.port);
+server.listen(process.env.PORTor config.port);
