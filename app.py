aac9b7d1d33bad7992e3a49855e106d56cb56a27
Elias Fakoorian <98291494+seyedeliasfakoorian@users.noreply.github.com>
2023-10-26 16:04:28 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import system
+
+
+import website-breaker
+# website-breaker? by Elias Fakoorian - eliasfakooriancom.godaddysites.com - github.com/seyedeliasfakoorian
+
+import alloy-proxy
+
+system:(alloy-proxy)info-website-breaker
+break-system
